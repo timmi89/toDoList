81542b9bde14be6f670a73eca630d45c8bf21cdc
Jon Sweeney and timmi mcguire <jonsweeney26@gmail.com>
2016-01-25 09:18:29 -0800

    will give functions to add people to addy book

--- spec/specs.js
+++ spec/specs.js
@@ -1,8 +1,15 @@
 describe('Contact', function() {
+
   it("creates a new contact with the given specifications", function() {
     var testContact = new Contact("Rita","Moreno");
     expect(testContact.firstName).to.equal("Rita");
     expect(testContact.lastName).to.equal("Moreno");
     expect(testContact.addresses).to.eql([]);
   });
+
+  it("adds the full Name to a contact", function() {
+    var testContact = new Contact("Bill","Clinton");
+    expect(testContact.fullName()).to.equal("Bill Clinton");
+  });
+
 });
