917029d261c05dd3e2625e417fa631cb3bc47f79
Jon Sweeney and timmi mcguire <jonsweeney26@gmail.com>
2016-01-25 16:40:40 -0800

    change click

--- js/scripts.js
+++ js/scripts.js
@@ -11,44 +11,15 @@ $(function(){
     var inputtedTaskDescription = $("input#newTaskDescription").val();
     var newTask = new Task(inputtedTaskName, inputtedTaskDescription);
 
-    $("form#tasks").append("<p><input type='checkbox' class='isChecked'><span>" + newTask.name + "<br></span></p>");
+    $("form#tasks").append("<p><input type='checkbox' id='newTask.name'>" + newTask.name + "<br></p>");
 
     $("input#newTaskName").val("");
     $("input#newTaskDescription").val("");
 
   });
 
-  $("#tasks").on("click", function() {
+  $("#tasks").click(function() {
     $("input:checked").parent().remove();
   });
 
 });
-
-
-
-//
-// Contact.prototype.fullName = function() {
-//   return this.firstName + " " + this.lastName;
-// }
-//
-// $(function() {
-//   $("form#new-contact").submit(function(event) {
-//
-//       var inputtedFirstName = $("input#new-first-name").val();
-//       var inputtedLastName = $("input#new-last-name").val();
-//       var newContact = new Contact(inputtedFirstName, inputtedLastName);
-//
-//       $("ul#contacts").append("<li><span class='contact'>" + newContact.fullName() + "</span></li>");
-//
-//       $("input#new-first-name").val("");
-//       $("input#new-last-name").val("");
-//
-//       $(".contact").last().click(function() {
-//         $("#show-contact").show();
-//         $("#show-contact h2").text(newContact.fullName());
-//         $(".first-name").text(newContact.firstName);
-//         $(".last-name").text(newContact.lastName);
-//       });
-//       event.preventDefault();
-//     });
-//   });
